9ade1a31839af03a5aa9f0adc0d8c9252cc2e54c
kingold5 <xingran_wang@icloud.com>
2020-04-25 20:14:13 +0200

    delete c++ console code

--- Src/main.cpp
+++ Src/main.cpp
@@ -11,19 +11,4 @@ int main(int argc, char* argv[]) {
     w.show();
 
     return app.exec();
-
-    /*
-    int hour, minute, second;
-    cout << "Please input hour:" << endl;
-    cin >> hour;
-    cout << "Please input minute:" << endl;
-    cin >> minute;
-    cout << "Please input second:" << endl;
-    cin >> second;
-
-    Countdown timer(hour, minute, second);
-    timer.run();
-
-    return 0;
-    */
 }
