315dc4c2d9e428024f84f4ee3e3ab28266d8c979
kingold5 <xingran_wang@icloud.com>
2020-05-09 16:40:48 +0200

    add struct Projects

---
+++ Src/projectsstruct.h
@@ -0,0 +1,17 @@
+#ifndef PROJECTSTRUCT_H
+#define PROJECTSTRUCT_H
+
+#include <QString>
+
+struct Projects{
+    int id;
+    QString name;
+    QString duration;
+    double hour;
+    double minute;
+    double second;
+    QString createDate;
+};
+
+
+#endif // PROJECTSTRUCT_H
