8712e11153304bb1c8408a29fc9bb57c077a6feb
kingold5 <xingran_wang@icloud.com>
2020-05-25 14:38:44 +0200

    add fonts

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <QApplication>
+#include <QFontDatabase>
 #include "mainWindow.h"
 
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    int id = QFontDatabase::addApplicationFont("../Ubuntu/Ubuntu-R.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont font(family, 12);
+    app.setFont(font);
 
     MainWindow w;
     w.show();
