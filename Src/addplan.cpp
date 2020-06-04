e9e58128e2883b76c6170e8281f483d00f9bffa7
kingold5 <xingran_wang@icloud.com>
2020-06-04 19:17:13 +0200

    use MySpinBox

--- Src/addplan.cpp
+++ Src/addplan.cpp
@@ -1,11 +1,20 @@
 #include "addplan.h"
 #include "ui_addplan.h"
+#include "myspinbox.h"
 
-AddPlan::AddPlan(QWidget *parent) :
+AddPlan::AddPlan(QDomDocument doc, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::AddPlan)
+    ui(new Ui::AddPlan),
+    docRef(doc)
 {
     ui->setupUi(this);
+    ui->mySpinBoxHour->setRange(0, 99);
+    ui->mySpinBoxMin->setRange(0, 59);
+    ui->mySpinBoxSec->setRange(0, 59);
+    ui->mySpinBoxHour->setWrapping(true);
+    ui->mySpinBoxMin->setWrapping(true);
+    ui->mySpinBoxSec->setWrapping(true);
+
 }
 
 AddPlan::~AddPlan()
--- Src/addplan.h
+++ Src/addplan.h
@@ -2,6 +2,7 @@
 #define ADDPLAN_H
 
 #include <QDialog>
+#include <QDomDocument>
 
 namespace Ui {
 class AddPlan;
@@ -12,11 +13,12 @@ class AddPlan : public QDialog
     Q_OBJECT
 
 public:
-    explicit AddPlan(QWidget *parent = nullptr);
+    explicit AddPlan(QDomDocument doc, QWidget *parent = nullptr);
     ~AddPlan();
 
 private:
     Ui::AddPlan *ui;
+    QDomDocument docRef;
 };
 
 #endif // ADDPLAN_H
